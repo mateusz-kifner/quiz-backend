009c568fa37dff0a174f8e97479620451f492453
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2019-09-19 21:52:07 +0200

    Heroku fix

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const app = express();
 
 const server = http.createServer(app);
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 app.get("/", (req, res, next) => {
   res.send("test node ");
