430d5030d39bc27a16eb4e3ca1a391c256f4c3b3
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2019-09-19 21:36:43 +0200

    Heroku changes

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const port = process.env.port || 3000;
+
 app.use(bodyParser.json());
 
 app.get("/", (req, res, next) => {
   res.send("test");
 });
 
-app.listen(80);
+app.listen(port);
