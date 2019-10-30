0dfaa6253ac1ce28018743660797c2e5b74b5ac3
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2019-10-30 19:02:23 +0100

    cors fix

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization,Set-Cookie"
   );
-  res.header("Access-Control-Allow-Credentials", "true");
+  // res.header("Access-Control-Allow-Credentials", "true");
   if (req.method === "OPTIONS") {
     res.header(
       "Access-Control-Allow-Methods",
--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const http = require("http");
 const app = require("./app");
 
 const server = http.createServer(app);
-
+console.log(process.env.PORT || 3001);
 server.listen(process.env.PORT || 3001);
